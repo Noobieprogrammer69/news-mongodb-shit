d4fecf132131004e63fda2156b0222894265b04e
Noobieprogrammer69 <joververgara1@gmail.com>
2022-05-28 19:38:50 +0800

    first commit

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseUrl: 'https://news-mongodb-shit.vercel.app'
+    baseUrl: 'https://mongo-db-loginproject.herokuapp.com'
 });
 
 export default instance;
